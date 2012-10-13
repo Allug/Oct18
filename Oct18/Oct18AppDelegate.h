69cbf215c3a6302c17df59f943fcf5b5ec24af5e
Allug <phillipgulla@gmail.com>
2012-10-13 19:06:23 -0400

    Initial Commit

---
+++ Oct18/Oct18AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Oct18AppDelegate.h
+//  Oct18
+//
+//  Created by Phillip Gulla on 10/13/12.
+//  Copyright (c) 2012 Allug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Oct18AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Oct18/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Oct18
+//
+//  Created by Phillip Gulla on 10/13/12.
+//  Copyright (c) 2012 Allug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Oct18AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([Oct18AppDelegate class]));
+	}
+}
